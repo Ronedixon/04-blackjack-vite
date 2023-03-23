3ec4531f9d95d0eae3666cbca075f32a1798f50f
Ronald Rodriguez <ronedixon@gmail.com>
2023-03-23 12:02:04 -0500

    tercer commit refactorizacion de modulos.

---
+++ src/usecases/index.js
@@ -0,0 +1,6 @@
+
+export { crearDeck } from './crear-deck';
+export { pedirCarta} from'./pedir-carta';
+export { valorCarta } from './valor-carta';
+export {crearCartaHtml} from './crear-carta-hmtl';
+export {turnoComputadora} from './turno-computadora';
